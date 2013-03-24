b289f70fcc1785085578b134dfa26b0473124453
Ronan <ronan.farrell@hotmail.com>
2013-03-24 20:09:51 +0000

    Added Havok and pre compiled headers [RRF-1]

---
+++ RWM_P5/RWM_P5/include/Physics.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "stdafx.h"
+
+class Physics
+{
+	
+	hkpWorld* physicsWorld;
+	hkJobQueue* jobQueue;
+	hkJobThreadPool* threadPool;
+
+
+public:
+	
+
+	Physics(void);
+	~Physics(void);
+
+	hkpWorld* GetPhysicsWorld(){return physicsWorld;}
+	void SetUp(void);
+	
+	void Simulate(float);
+	void TidyUp(void);
+	
+};
